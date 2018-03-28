891cf8798a3f9cf1a37c6e42aaf53f0e0f6ff7dd
Benjamin LeBrun <bl.lebrun@gmail.com>
2018-03-28 12:48:39 -0600

    Finished second question

--- HW5-2/traffic_light_controller.vhd
+++ HW5-2/traffic_light_controller.vhd
@@ -28,21 +28,62 @@ entity traffic_light_controller is
 end traffic_light_controller;
 
 architecture Behavioral of traffic_light_controller is
-  type side_street_state is (gs, ys, rs);
-  type main_street_state is (gm, ym, rm);
-  signal side_reg, side_next: side_street_state;
-  signal main_reg, main_next: main_street_state;
+  type street_state is (s0, s1, s2, s3);
+  signal state_reg, state_next: street_state;
 begin
   -- state reg
-  process(clk, reset)
+  process(clk)
   begin
     if(clk'event and clk='1') then
-      side_reg <= side_next;
-      main_reg <= main_next;
+      state_reg<=state_next;
     end if;
   end process;
 
   -- next state/output logic
-  
+process(state_reg, ns, ew)
+begin
+  -- defaults
+  state_next<=state_reg;
+  gm<='0';
+  ym<='0';
+  rm<='0';
+  gs<='0';
+  ys<='0';
+  rs<='0';
+  case state_reg is
+    when s0 =>
+      rs<='1';
+      gm<='1';
+      if ew='1' then
+        state_next<=s1;
+      else
+        state_next<=s0;
+      end if;
+    when s1=>
+      rs<='1';
+      ym<='1';
+      if ew='1' then
+        state_next<=s2;
+      else
+        state_next<=s0;
+      end if;
+    when s2=>
+      gs<='1';
+      rm<='1';
+      if ns='1' then
+        state_next<=s3;
+      else
+        state_next<=s2;
+      end if;
+    when s3=>
+      ys<='1';
+      rm<='1';
+      if ns='1' then
+        state_next<=s0;
+      else
+        state_next<=s2;
+      end if;
+    end case;
+  end process;
 
 end Behavioral;
