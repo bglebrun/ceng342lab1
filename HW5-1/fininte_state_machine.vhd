77380521e5a0259fe9b24deaa126573d999b7b6f
Benjamin LeBrun <bl.lebrun@gmail.com>
2018-03-28 01:10:23 -0600

    Formatting

--- HW5-1/fininte_state_machine.vhd
+++ HW5-1/fininte_state_machine.vhd
@@ -20,15 +20,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity fininte_state_machine is
     Port ( x : in  STD_LOGIC;
            clk, reset: in STD_LOGIC;
